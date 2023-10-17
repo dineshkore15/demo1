7fdc45e1a401c11bcd2a6a34d59a8c99e3e6aa91
dineshkore15 <130357476+dineshkore15@users.noreply.github.com>
2023-10-17 15:16:02 +0200

    Create practices1.py

---
+++ practices1.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://www.flipkart.com/')
+time.sleep(5)
+driver.find_element(By.XPATH,'/html/body/div[3]/div/span').click()
+driver.find_element(By.XPATH,'//*[@id="container"]/div/div[1]/div/div/div/div/div[1]/div/div[1]/div/div[1]/header/div[1]/div[2]/form/div/div/input').send_keys("One plus mobile ")
+print("Updated Code")
+
+driver.find_element(By.XPATH,"/html/body/div[3]/div/span").click()
+driver.find_element(By.XPATH,"/html/body/div[3]/div/span").click()
+driver.find_element(By.XPATH,"/html/body/div[3]/div/span").click()
+driver.find_element(By.XPATH,"/html/body/div[3]/div/span").click()
